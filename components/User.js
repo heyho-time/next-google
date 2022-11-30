4ff56c8db8f932b6f95a2155f65a76aac0efbb7c
성해호 <hhseong@tidesquare.com>
2022-11-30 13:34:39 +0900

    fix lint/prettier

--- components/User.js
+++ components/User.js
@@ -1,11 +1,16 @@
-import {useSession, signIn, signOut} from 'next-auth/react'
+import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function User() {
-  const {data:session} = useSession()
-  if(session){
-    return(
+  const { data: session } = useSession()
+  if (session) {
+    return (
       <>
-        <img src={session.user.image} />
+        <img
+          onClick={signOut}
+          src={session.user.image}
+          alt="user-image"
+          className={`h-10 w-10 rounded-full hover:bg-gray-200 cursor-pointer p-1 `}
+        />
       </>
     )
   }
